d28c95602cbe5424162b1ac04d0bb3517011640b
StarsCat <starscat@163.com>
2019-05-21 22:25:53 +0800

    Initial commit

---
+++ src/test/Test1.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        int b=120/100;
+        double a=120/100;
+
+        System.out.println("b="+b+";a="+a);
+
+    }
+}
